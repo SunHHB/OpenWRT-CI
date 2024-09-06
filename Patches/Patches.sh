0f5e46b10a1885ac3f22a6aedccd4a14e75b04c5
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-09-06 15:12:54 +0800

    Update Patches.sh

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' ./wrt/feeds/packages/net/tailscale/Makefile
 if [[ $WRT_REPO == *"lede"* ]]; then
 	sed -i "/#src/d" ./wrt/feeds.conf.default
 	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
